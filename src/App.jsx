1ebe432c011cb8d473d410ca290e82243660f291
Jean Alvarez <blackraisondetre126@gmail.com>
2025-06-26 09:47:35 -0500

    Handle name change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import { useState } from "react"
 
 function App() {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", id: 1 }
+  ]);
+  const [newName, setNewName] = useState("");
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  }
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <form>
+        <div className="row">
+          <label htmlFor="name">Name:</label>
+          <input onChange={handleNameChange} type="text" id="name" name="name" value={newName} />
+        </div>
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  )
 }
 
 export default App
