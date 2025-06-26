354451c9cd5f4eadbd98ef0c63c4152a1cacd6aa
Jean Alvarez <blackraisondetre126@gmail.com>
2025-06-26 10:04:04 -0500

    Add filter input

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react"
 
 function App() {
+
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "39-44-52323", id: 0 }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]);
+
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -13,6 +19,10 @@ function App() {
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
+  }
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
   }
 
   const handleSubmit = (event) => {
@@ -50,6 +60,9 @@ function App() {
         <button type="submit">Add</button>
       </form>
       <h2>Numbers</h2>
+      <p>filter by name:
+        <input type="text" placeholder="Arto Hellas" value={filter} onChange={handleFilterChange}></input>
+      </p>
       <ul>
         {persons.map((person) => <li key={person.id}>{person.name} {person.number}</li>)}
       </ul>
